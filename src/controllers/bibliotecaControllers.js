efdd00208479d2fdc749d058c126d548efd67fce
Maikol <bustosmaikol.05@gmail.com>
2025-09-27 15:23:07 -0500

    Add Supabase integration and update user authentication methods

--- src/controllers/bibliotecaControllers.js
+++ src/controllers/bibliotecaControllers.js
@@ -2,6 +2,7 @@
 async function Login (req, res) {
   try {
     
+    
   } catch (err) {
     console.error(err)
     res.status(500).json({ error: 'papi error' })
@@ -10,7 +11,7 @@ async function Login (req, res) {
 
 async function Register (req, res) {
   try {
-    
+    // llamada al servicio de registro de usuario de supabase
   } catch (err) {
     console.error(err)
     res.status(500).json({ error: 'papi error' })
