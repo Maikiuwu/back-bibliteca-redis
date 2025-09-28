d09a541316cb2c1e6dec752a4e32138162f40da4
Maikol <bustosmaikol.05@gmail.com>
2025-09-28 16:14:39 -0500

    Update Redis connection to use environment variables for configuration

--- src/redisConection.js
+++ src/redisConection.js
@@ -1,20 +1,22 @@
-
-import { createClient } from 'redis';
+import { createClient } from 'redis'
+import dotenv from 'dotenv'
+dotenv.config()
 
 export const client = createClient({
-    username: 'default',
-    password: 'MAYiflxHFSCNyTnb5U6BWa9ccqZdOEaT',
+    username: process.env.USERNAME_REDIS,
+    password: process.env.PASSWORD_REDIS,
     socket: {
-        host: 'redis-15690.c281.us-east-1-2.ec2.redns.redis-cloud.com',
-        port: 15690
+        host: process.env.HOST_REDIS,
+        port: Number(process.env.PRT_REDIS)
     }
-});
+})
 
-client.on('error', err => console.log('Redis Client Error', err));
+client.on('error', err => console.log('Redis Client Error', err))
 
+/*
 await client.connect();
 
 await client.set('foo', 'bar');
 const result = await client.get('foo');
 console.log(result)  // >>> bar
-
+*/
